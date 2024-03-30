879d4c523fa72c2a5ea74eaf86d3fd868a630b65
Iuri Sant <iuri.s.goes@gmail.com>
2024-03-30 19:03:41 -0300

    Edit: Reorganização das pastas

--- db.php
+++ db.php
@@ -10,35 +10,3 @@ try {
     <script> console.log(". $e->getMessage() .");</script>
   ";
 }
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  if(!empty($_POST["title"]) && !empty($_POST["description"])) {
-    $title = $_POST["title"];
-    $description = $_POST["description"];    
-    try {
-      $sql = "INSERT INTO posts (titulo, descricao) VALUES (:titulo, :descricao)";
-
-      $stmt = $conn->prepare($sql);
-      $stmt->bindParam(':titulo', $title);
-      $stmt->bindParam(':descricao', $description);
-      $stmt->execute();
-
-      echo "
-        <script>
-        document.getElementById('containerModal').style.display = 'none'
-          console.log('Inserção bem-sucedida!')
-        </script>
-      ";
-    } catch(PDOException $e) {
-      echo "
-        <script>console.error(". $e .")</script>
-      ";
-    }
-    header("location: index.php");
-  }else{
-    echo "
-      <script> console.warn('Preencha todos os campos!');</script>
-    ";
-    header("location: index.php");
-  }
-}
